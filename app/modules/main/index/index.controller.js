927fd5b5e9b36653ea947027a3f7af46226d35f8
Stenkilde <hello@stenkil.de>
2016-04-26 18:17:31 +0200

    Added new images, updating paths

--- app/modules/main/index/index.controller.js
+++ app/modules/main/index/index.controller.js
@@ -9,10 +9,18 @@
 	function Index() {
 		/*jshint validthis: true */
 		var vm 		= this;
+		vm.partners = [];
+		vm.clubs	= [];
 
 		activate();
 
 		function activate() {
+			$http.get('partners.json').then(function(response) {
+				vm.partners = response.data;
+			});
+			$http.get('clubs.json').then(function(response) {
+				vm.clubs = response.data;
+			});
 		}
 
 	}
