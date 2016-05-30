f98ede35d95d01c89f6de521252fcd786a5136b0
Oliver Stenkilde <hello@stenkil.de>
2016-05-30 19:11:23 +0200

    Weekend progress

--- app/modules/shared/topbar/topbar.controller.js
+++ app/modules/shared/topbar/topbar.controller.js
@@ -6,12 +6,13 @@
 		.controller('Topbar', Topbar);
 
 	/* @ngInject */
-	function Topbar($http) {
+	function Topbar($http, $modal) {
 		/*jshint validthis: true */
 		var vm 			= this;
 		vm.clubs		= [];
 		vm.menuState	= false;
 		vm.acState		= false;
+		vm.openModal	= openModal;
 
 		activate();
 
@@ -19,6 +20,20 @@
 			$http.get('clubs.json').then(function(response) {
 				vm.clubs = response.data;
 			});
+		}
+
+		function openModal() {
+			$modal.open({
+		      templateUrl: 'modules/shared/modals/gallery.modal.html',
+			  controller: 'ModalCtrl',
+			  controllerAs: 'modal',
+			  windowClass: 'collapsed',
+			  resolve: {
+                    items: function() {
+						return [];
+                    }
+                }
+		    });
 		}
 
 	}
